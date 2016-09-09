ab54031bbc204f6430780f61f3bcddc10ab5a4fe
Knils <silversnake001@aol.com>
2016-09-09 16:56:35 -0500

    First commit

---
+++ Assets/Editor/AssetbundlesMenuItems.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System;
+
+namespace AssetBundles
+{
+	public class AssetBundlesMenuItems
+	{	
+		[MenuItem ("Assets/Build AssetBundles")]
+		static public void BuildAssetBundles ()
+		{
+            string outputPath = "AssetBundles";
+
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+
+            try
+            {
+                BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, EditorUserBuildSettings.selectedStandaloneTarget);
+
+                //Add .unity3d to AssetBundles and deletes unnecessary files
+                string[] files = Directory.GetFiles(outputPath);
+
+                foreach (string path in files)
+                {                    
+                    if (Path.GetFileName(path) == "AssetBundles" || path.EndsWith(".manifest"))
+                    {
+                        File.Delete(path);
+                    }
+                    else if (!Path.HasExtension(path))
+                    {
+                        string pathPlusUnity = path + ".unity3d";
+
+                        if (File.Exists(pathPlusUnity))
+                        {
+                            File.Delete(pathPlusUnity);
+                        }
+
+                        File.Move(path, pathPlusUnity);
+                    }
+                }
+
+                Debug.Log("Build complete! AssetBundles can be found in the 'Tabletop Simulator - Modding/AssetBundles' folder.");
+            }
+            catch(Exception e)
+            {
+                Debug.LogError("Error building AssetBundles: " + e.Message);
+            }           
+        }
+	}
+}
